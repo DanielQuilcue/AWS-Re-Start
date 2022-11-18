72d9e2d717613342194028df1a8051a2abbf3040
DanielQuilce <danielquilcue18@gmail.com>
2022-11-17 19:41:48 -0500

    Modules to retrieve information about insulin

---
+++ Modulo_controlador/jsonFileHandler.py
@@ -0,0 +1,14 @@
+"""
+    creará un módulo que lee el archivo JSON y devuelve el documento JSON.
+"""
+
+import json
+
+def readJsonFile(fileName):
+    data = ""
+    try:
+        with open(fileName) as json_file:
+            data = json.load(json_file)
+    except IOError:
+        print("Could not read file")
+    return data
